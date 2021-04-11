ed6fd408812a31469667a7fadffa258c10878936
Ruben <just14zy@gmail.com>
2021-04-12 01:02:53 +0300

    upd

--- js/bowl-mixer.js
+++ js/bowl-mixer.js
@@ -1,35 +1,143 @@
 
 
+    currentBlock = 1;
+    bowl = [];
+    bowlToppings = [];
+    bowlSauce = [];
+    bowlPosipka = [];
+    bowlDesc = "";
+    bowlPrice = 0;
     
-    // buttonClicked = function (x) {
-      
-      
+
+    $('.topping').click(function(){
+        $(this).toggleClass('active');
+    })
+
+    $('.sauce').click(function(){
+        $(this).toggleClass('active');
+    })
+
+    $('.posipka').click(function(){
+        $(this).toggleClass('active');
+    })
+
+    addItem = function (item, price)  {
         
-    //     // $("#button-1").removeClass("animate__animated animate__zoomInUp");
-    //     // $("#button-1").addClass("animate__animated animate__bounceOutUp");
+        if (bowl.length < 1) {
+            $("#block1-choose").html(item);
+        }
 
-    //     // animateCSS('#button1', 'bounce');
+        bowl.push(item);
+        bowlPrice = bowlPrice + price;
+     
+        updateBowl();
+        nextBlock();
+    }
+
+    
+    addTopping = function (item, price) {
+
+        if ( jQuery.inArray(item, bowlToppings) == -1 ) {
+            bowlToppings.push(item);
+            bowlPrice = bowlPrice + price;
+        } else {
+            bowlToppings.splice(jQuery.inArray(item, bowlToppings), 1)
+            bowlPrice = bowlPrice - price
+        }
         
+        bowl[2] = bowlToppings;
+        updateBowl();
 
-    //     // $("#button-2").removeClass("animate__animated animate__zoomInUp");
-    //     // $("#button-2").addClass("animate__animated animate__fadeOut");
+    }
 
-    //     // $("#button-3").removeClass("animate__animated animate__zoomInUp");
-    //     // $("#button-3").addClass("animate__animated animate__fadeOut");
 
-      
+    addSauce = function (item, price) {
 
-    //   // document.getElementById("button1").innerHTML = "Мясо";
+        if ( jQuery.inArray(item, bowlSauce) == -1 ) {
+            bowlSauce.push(item);
+            bowlPrice = bowlPrice + price;
+        } else {
+            bowlSauce.splice(jQuery.inArray(item, bowlSauce), 1)
+            bowlPrice = bowlPrice - price
+        }
+        
+        bowl[3] = bowlSauce;
+        updateBowl();
 
-    // //   document.getElementById("title").innerHTML = "Выберите протеин:";
-    //   // document.getElementById("button1").innerHTML = "Мясо";
-    //   // document.getElementById("button2").innerHTML = "Рыба";
-    // //   document.getElementById("desc").innerHTML = "Шаг 2 из 4";
+    }
 
-    //   showBlock(2);
+    addPosipka = function (item, price) {
 
-    // }
+        if ( jQuery.inArray(item, bowlPosipka) == -1 ) {
+            bowlPosipka.push(item);
+            bowlPrice = bowlPrice + price;
+        } else {
+            bowlPosipka.splice(jQuery.inArray(item, bowlPosipka), 1)
+            bowlPrice = bowlPrice - price
+        }
+        
+        bowl[4] = bowlPosipka;
+        updateBowl();
 
+    }
+
+
+    
+
+    updateBowl = function() {
+        $("#totalSum").html(bowlPrice);
+
+        if (bowl.length == 1) {
+            bowlDesc = "Основа: " + bowl[0];
+        }
+
+        if (bowl.length == 2) {
+            bowlDesc = "Основа: " + bowl[0] + "<br>" + "Протеин: " + bowl[1]
+        }
+
+        if (bowl.length == 3) {
+            bowlDesc = "Основа: " + bowl[0] + "<br>" + "Протеин: " + bowl[1] + "<br>" + "Топпинги: " + bowl[2];
+        }
+
+        if (bowl.length == 4) {
+            bowlDesc = "Основа: " + bowl[0] + "<br>" + "Протеин: " + bowl[1] + "<br>" + "Топпинги: " + bowl[2] + "<br>" + "Соусы: " + bowl[3];
+        }
+
+        if (bowl.length == 5) {
+            bowlDesc = "Основа: " + bowl[0] + "<br>" + "Протеин: " + bowl[1] + "<br>" + "Топпинги: " + bowl[2] + "<br>" + "Соусы: " + bowl[3] + "<br>" + "Посыпка: " + bowl[4];
+        }
+
+        $("#totalOrder").html(bowlDesc);
+    }
+
+    toppingsFinished = function() {
+        if (bowlToppings.length !=0) {
+            nextBlock();
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Нужно добавить топпинги',
+                text: 'Добавьте хотя бы один топпинг',
+                confirmButtonText: 'Хорошо'
+              })   
+        }
+    };
+
+    order = function() {
+        Swal.fire({
+            icon: 'success',
+            title: 'Ваш заказ',
+            html: bowlDesc,
+            confirmButtonText: 'Отправить'
+          })   
+
+
+    }
+    
+    nextBlock = function() {
+        currentBlock = currentBlock + 1;
+        showBlock(currentBlock);
+    }
 
     showBlock = function (x) {
         $("#block-1").hide();
@@ -40,6 +148,22 @@
         $("#block-"+x).show();
     }
 
+
+    refresh = function() {
+        currentBlock = 1;
+        bowl = [];
+        bowlToppings = [];
+        bowlSauce = [];
+        bowlPosipka = [];
+        bowlDesc = "Выберите основу и ингредиенты для приготовления боула";
+        bowlPrice = 0;
+                
+        updateBowl();
+        showBlock(1);
+    }
+
+    
+    
+    // showBlock(4);
       
 
-//   showBlock(2);
